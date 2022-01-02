6eee680721ba6a58d06c368527c95b95496eb451
m-minkevich <55951151+m-minkevich@users.noreply.github.com>
2022-01-02 18:43:14 +0300

    Initial Commit

---
+++ Homework TrackingTests/Homework_TrackingTests.swift
@@ -0,0 +1,33 @@
+//
+//  Homework_TrackingTests.swift
+//  Homework TrackingTests
+//
+//  Created by Matt Minkevich on 02/01/2022.
+//
+
+import XCTest
+@testable import Homework_Tracking
+
+class Homework_TrackingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
